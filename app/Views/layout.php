7fa0c5e520fdf7a87f7e6e83408ccaf91924e901
Kenpradana <kenpradana8880@gmail.com>
2025-07-04 17:23:41 +0700

    Hasil UAS

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -98,7 +98,7 @@ if (uri_string() != "") {
     <?= $this->include('components/footer') ?>
 
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
+    
     <!-- Vendor JS Files -->
     <script src="<?php echo base_url() ?>NiceAdmin/assets/vendor/apexcharts/apexcharts.min.js"></script>
     <script src="<?php echo base_url() ?>NiceAdmin/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
